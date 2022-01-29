a959deaab70d5f5ae1c7267a254290514e726294
Ryan H Lewis <ryanhhlewis@gmail.com>
2022-01-29 12:01:02 -0600

    Template 2D

---
+++ Assets/MinigameStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MinigameStart : MonoBehaviour
+{
+    public TMP_Text minigameText;
+
+    // Applies an upwards force to all rigidbodies that enter the trigger.
+    void OnTriggerStay(Collider other)
+    {
+        minigameText.text = "Press E to start minigame.";
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Top Down Movement, used for OUTSIDE of minigames. */
+
+public class Move: MonoBehaviour {
+     
+     public float speed = 15;
+     private Vector2 velocity;
+     private Rigidbody2D rb;
+     private void Awake() {
+          rb = GetComponent<Rigidbody2D>();
+          velocity = Vector2.zero;
+     }
+     private void Update() {
+          velocity.x = Input.GetAxisRaw("Horizontal");
+          velocity.y = Input.GetAxisRaw("Vertical");
+     }
+     private void FixedUpdate() {
+           rb.MovePosition(rb.position + 
+                (velocity.normalized * speed *
+                 Time.fixedDeltaTime)); 
+     }
+}
