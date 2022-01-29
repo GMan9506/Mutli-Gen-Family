5addfaf427498c19dadffb2a101ccb79e1fce555
Ryan H Lewis <ryanhhlewis@gmail.com>
2022-01-29 12:18:12 -0600

    Checkpoint Trigger 2D

--- Assets/MinigameStart.cs
+++ Assets/MinigameStart.cs
@@ -7,9 +7,15 @@ public class MinigameStart : MonoBehaviour
 {
     public TMP_Text minigameText;
 
-    // Applies an upwards force to all rigidbodies that enter the trigger.
-    void OnTriggerStay(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Player entered minigame trigger.");
         minigameText.text = "Press E to start minigame.";
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Player left minigame trigger.");
+        minigameText.text = "";
     }
 }
