bb5b668b0012d358dad09be730a7d9ff71f558de
Ryan H Lewis <ryanhhlewis@gmail.com>
2022-01-30 02:46:03 -0600

    Movement Sprite

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -10,15 +10,24 @@ public class Move: MonoBehaviour {
      public float speed = 15;
      private Vector2 velocity;
      private Rigidbody2D rb;
+     private SpriteRenderer charact;
+
      private void Awake() {
           rb = GetComponent<Rigidbody2D>();
           velocity = Vector2.zero;
+          charact = GetComponent<SpriteRenderer>();
+
      }
      private void Update() {
         if (!canMove)
             return;
           velocity.x = Input.GetAxisRaw("Horizontal");
           velocity.y = Input.GetAxisRaw("Vertical");
+          if(velocity.x > 0) {
+               charact.flipX = true;
+          } else if(velocity.x < 0) {
+               charact.flipX = false;
+          }
      }
      private void FixedUpdate() {
            rb.MovePosition(rb.position + 
